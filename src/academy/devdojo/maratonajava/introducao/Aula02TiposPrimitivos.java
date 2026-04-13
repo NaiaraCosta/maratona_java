e28c8cbbe669529af41768cfcaca2adb797285a6
NaiaraCosta <naiaradecosta@hotmail.com>
2026-04-13 16:12:48 -0300

    Update Aula02TiposPrimitivos.java to change character initialization from UNICODE to ASCII and update 04_conceitos.md to add a new Unicode table image

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -21,13 +21,14 @@ public class Aula02TiposPrimitivos {
         short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
-        //char caractere = '\u0041';  // UNICODE
-        char caractere = 87;          // ASCII
+        char caractere = '\u0041';  // UNICODE
+        char character = 87;       // ASCII
         String nome = "Goku";
 
         System.out.println("A idade é " + idade + " anos");
         System.out.println(falso);
-        System.out.println("char " + caractere);
+        System.out.println("char: " + caractere);
+        System.out.println("char_1: " + character);
         System.out.println(numeroGrande);
         System.out.println("Oi meu nome é " + nome);
     }
