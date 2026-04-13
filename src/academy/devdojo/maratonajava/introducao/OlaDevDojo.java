8ed760700ec3496912a7789cb3982d87721b28ea
Naiara Juliana Basso Costa <naiara.costa@aircompany.ai>
2026-04-13 09:13:20 -0300

    Aplicando .gitignore corretamente

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
+
+// Rode o comando: javac academy\\devdojo\\maratonajava\\introducao\\OlaDevDojo.java
+
+// Rode o comando: java academy.devdojo.maratonajava.introducao.OlaDevDojo
+
+// Lebre-se: toda a vez que o você atualiza o arquivo, é necessário rodar o comando "javac ..."
+// novamente para atualizar o arquivo *.class (bytecode)
