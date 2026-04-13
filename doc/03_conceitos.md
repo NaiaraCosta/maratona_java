0fdc06f384c80712133e0a33101f6f5da5aacce9
NaiaraCosta <naiaradecosta@hotmail.com>
2026-04-13 13:46:59 -0300

    Add package naming conventions and a new Java class example

--- doc/03_conceitos.md
+++ doc/03_conceitos.md
@@ -44,3 +44,18 @@ int b = 5;
 **System.out..println(a +b); // 15**
 
 **System.out.println(a >b); // true**
+
+
+## **Principais Convenções de Nomenclatura de Pacotes**
+
+
+-Domínio Invertido: Inicie com com, org, edu, ou código de país (br, us),
+seguido pelo domínio da empresa/autor (ex: com.google, br.com.empresa).
+
+-Letras Minúsculas: Use apenas minúsculas para evitar conflitos com nomes de classes.
+
+-Separação: Utilize pontos "." para separar os níveis hierárquicos,
+o que reflete a estrutura de pastas no sistema de arquivos.
+
+-Nome do Projeto/Recurso: Após o domínio, adicione o nome do projeto
+e o subpacote funcional (ex: br.com.meuprojeto.model, com.empresa.service).
---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,44 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}
+
+/*
+Rode os  comandos:
+-  dir
+-  dir -Recurse Aula01OlaDevDojo.java
+*/
+
+/*
+Saída:
+PS C:\Users\naiara.costa\IdeaProjects\maratona_java> dir -Recurse Aula01OlaDevDojo.java
+
+Directory: C:\Users\naiara.costa\IdeaProjects\maratona_java\src\academy\devdojo\maratonajava\introducao
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+        -a---          13/04/2026    13:06            628 Aula01OlaDevDojo.java
+*/
+
+//Rode o  comando: javac src\academy\devdojo\maratonajava\introducao\Aula01OlaDevDojo.java
+
+// Rode o comando: java -cp src academy.devdojo.maratonajava.introducao.Aula01OlaDevDojo
+
+
+
+// Lebre-se: toda a vez que o você atualiza o arquivo, é necessário rodar o comando "javac ..."
+// novamente para atualizar o arquivo "*.class" (bytecode)
+
+
+
+// Isso é um comentário de linha
+
+
+
+/*
+    Isso é um comentário
+    de multiplas linhas
+*/
