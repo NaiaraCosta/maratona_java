94ef21a047d5eaecd3ea3d2300ebaf41fe485663
NaiaraCosta <naiaradecosta@hotmail.com>
2026-04-13 15:24:37 -0300

    Comment out example code in Aula02TiposPrimitivos.java for clarity

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,5 +1,15 @@
 package academy.devdojo.maratonajava.introducao;
 
+/*
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        System.out.println("A idade é " + idade + " anos");
+    }
+}
+*/
+
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
