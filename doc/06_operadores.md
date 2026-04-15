ffbd19debe95d6bfc90d5245a4d5750e57894eaa
NaiaraCosta <naiaradecosta@hotmail.com>
2026-04-15 15:32:37 -0300

    Add examples for increment operators and implement loop structures in new Java class

--- doc/06_operadores.md
+++ doc/06_operadores.md
@@ -6,6 +6,25 @@
 - Atribuição: =, +=, -=, *=, /=
 - Incremento/Decremento: ++, --
 
+----------------------------------------------------------------------------
+count++ (pós-incremento): primeiro usa o valor atual, depois incrementa.
+
+int count = 5;
+int x = count++;
+
+System.out.println(x);     // 5
+System.out.println(count); // 6
+
+-----------------------------------
+++count (pré-incremento): primeiro incrementa, depois usa o valor.
+
+int count = 5;
+int x = ++count;
+
+System.out.println(x);     // 6
+System.out.println(count); // 6
+----------------------------------------------------------------------------
+
 Exemplos:
 
 int a = 10;
---
+++ src/academy/devdojo/maratonajava/introducao/aulas/Aula05EstruturasDeRepeticao.java
@@ -0,0 +1,65 @@
+package academy.devdojo.maratonajava.introducao.aulas;
+
+public class Aula05EstruturasDeRepeticao {
+
+    public static void main(String[] args) {
+        // while, do while, for
+        int count = 0;
+        while (count < 10) {
+            System.out.println(++count);
+        }
+
+        count = 0;
+        do {
+            System.out.println("dentro do do-while " + ++count);
+        } while (count < 10);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("For " + i);
+        }
+
+
+        // Imprima todos os números pares de 0 até 1000000
+        for (int i = 1; i <= 1000000; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+        // Imprima os primeiros 25 numeros de um dado valor. Por exemplo, 50
+        int valorMax = 50;
+        for (int i = 0; i <= valorMax; i++) {
+            if (i > 25) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+
+
+        // Dado o valor de um carro, descubra em quantas ele pode ser parcelado
+        // Condição valorParcela >= 1000
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+
+
+        // Dado o valor de um carro, descubra em quantas ele pode ser parcelado
+        // Condição valorParcela >= 1000
+        double valorTotal1 = 30000;
+        for (int parcela = (int) valorTotal1; parcela >= 1; parcela--) {
+            double valorParcela = valorTotal1 / parcela;
+            if (valorParcela < 1000) {
+                continue;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+
+    }
+}
